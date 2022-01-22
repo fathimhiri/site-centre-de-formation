fdb01dd3c057a06c34754874ba5bddcdc45f6ebc
fathi <fathimhiri10@gmail.com>
2022-01-22 12:00:13 +0100

    some cmodddddrdgggdr

--- src/Controller/ForgPassController.php
+++ src/Controller/ForgPassController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LoginController extends AbstractController
+class ForgPassController extends AbstractController
 {
     /**
-     * @Route("/login", name="login")
+     * @Route("/forg/pass", name="forg_pass")
      */
     public function index(): Response
     {
-        return $this->render('login/login.html.twig', [
-            'controller_name' => 'LoginController',
+        return $this->render('forg_pass/forg_pass.html.twig', [
+            'controller_name' => 'ForgPassController',
         ]);
     }
 }
